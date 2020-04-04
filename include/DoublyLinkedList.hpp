a3abfef0fd6bd51ed3a7b639117462c8e7d6fda7
Nikhilesh Gautam <nikhilgautam94@gmail.com>
2020-04-04 05:47:02 +0530

    Doubly Linked List Added

--- include/DoublyLinkedList.hpp
+++ include/DoublyLinkedList.hpp
@@ -1,8 +1,7 @@
 #pragma once
-#include<iostream>
-#include<vector>
+#include "LinkedList.hpp"
 
-class LinkedList
+class DoublyLinkedList
 {
 private:
 protected:
@@ -11,59 +10,91 @@ protected:
     private:
         int _data_{int()};
         Node *_next_{nullptr};
+        Node *_prev_{nullptr};
     public:
         Node(){}
         Node(int _data_value_):_data_(_data_value_){}
+        ~Node(){}
         int & data(){return this->_data_;}
         Node * & next(){return this->_next_;}
+        Node * & prev(){return this->_prev_;}
     };
+    Node *_head_{nullptr};
     void _delete_(Node *current_node)
     {
         if(current_node == nullptr) return;
         _delete_(current_node->next());
         delete current_node;
     }
-    Node *_head_{nullptr};
 public:
     enum InsertPosition{Begin, End};
-    LinkedList(){}
-    LinkedList(const std::vector<int> &_vector_, InsertPosition _position_ = InsertPosition::End)
+    DoublyLinkedList(){}
+    DoublyLinkedList(const std::vector<int> &_vector_, InsertPosition _position_ = InsertPosition::End)
     {
         for(auto element : _vector_)
             this->insert(element,_position_);
     }
-    ~LinkedList(){this->_delete_(this->_head_);}
+    ~DoublyLinkedList(){this->_delete_(this->_head_);}
 
     void insert(int _data_value_, InsertPosition _position_ = InsertPosition::End)
     {
         if(this->_head_ == nullptr)
         {
             this->_head_ = new Node(_data_value_);
+            this->_head_->prev() = nullptr;
             return;
         }
+
         if(_position_ == InsertPosition::Begin)
         {
             Node *temp_node = new Node(_data_value_);
             temp_node->next() = this->_head_;
+            temp_node->prev() = nullptr;
+            this->_head_->prev() = temp_node;
             this->_head_ = temp_node;
         }
+
         if(_position_ == InsertPosition::End)
         {
             auto iterator = this->_head_;
             while (iterator && iterator->next() != nullptr)
                 iterator = iterator->next();
-            iterator->next() = new Node(_data_value_);
+
+            auto temp_node = new Node(_data_value_);
+            temp_node->prev() = iterator;
+            iterator->next() = temp_node;
         }
+        return;
     }
 
     void traverse()
     {
         auto iterator = this->_head_;
+        std::cout << "{Current} {next} {prev}" << std::endl;
         while (iterator)
         {
-            std::cout<< iterator->data() << " ";
+            std::cout << "{" << iterator->data() << "} ";
+
+            if(iterator->next())
+                std::cout << "{" << iterator->next()->data() << "} ";
+            else
+                std::cout << "nullptr ";
+
+            if(iterator->prev())
+                std::cout << "{" << iterator->prev()->data() << "} ";
+            else
+                std::cout << "nullptr ";
+
+
+            std::cout << std::endl;
             iterator = iterator->next();
         }
+        return;
     }
-    friend std::ostream & operator << (std::ostream &output_stream,LinkedList &list){list.traverse();return output_stream;}
+
+    friend std::ostream & operator << (std::ostream &output_stream,DoublyLinkedList &list)
+    {
+        list.traverse();
+        return output_stream;
+    }
 };
